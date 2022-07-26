9dc16a4416608f0cc5e484fe07ff9db7d4cde579
Dponya <ealtynbek089@gmail.com>
2022-07-26 23:21:09 +0600

    change app impl, add instance of FromJSON

--- src/Network/RHC/Internal/Server.hs
+++ src/Network/RHC/Internal/Server.hs
@@ -11,11 +11,39 @@ import Network.Wai (
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
 import Data.ByteString.Builder (byteString, Builder)
-import Data.ByteString.Lazy (toStrict)
+import Data.ByteString.Lazy (toStrict, ByteString)
+import Data.Aeson (FromJSON, Object, decode)
+import Data.Aeson.Types
 
-{- app :: Application
-app req send = send (responseBuilder status200 []) <$> requestBodyReceiver req -}
--- app req send = send $ responseBuilder status200 [] $ reqPath req
+app :: Application
+app req send = do
+                let responseSender answer = send (responseBuilder status200 [] answer)
+                body <- requestBodyReceiver req
+                responseSender body
 
 requestBodyReceiver :: Request -> IO Builder
-requestBodyReceiver req = byteString . toStrict <$> lazyRequestBody req 
+requestBodyReceiver req = byteString . toStrict <$> lazyRequestBody req                        
+
+data RequestRHC prm = RequestRHC {
+        resVersion :: String,
+        method :: String,
+        params :: Maybe prm,  -- structure
+        reqId :: Maybe String -- note: should change request type to sum type
+                              -- between notification and ordinary request
+}
+
+data ResponseRHC res = Response {
+        reqVersion :: String,
+        result :: Maybe res, -- structure, determined structure by method
+        resError :: Maybe String, -- must be an object
+        resId :: Maybe String
+}
+
+instance FromJSON (RequestRHC a) where
+        parseJSON (Object v) = 
+                RequestRHC <$> v .: "jsonrpc"
+                           <*> v .: "method"
+                           <*> v .: "params"
+                           <*> v .: "id"
+
+-- test = run 3000 app
--- src/Network/RHC/Server.hs
+++ src/Network/RHC/Server.hs
@@ -6,18 +6,3 @@ newtype Methods = Methods [(MethodName, Method)]
 
 runRHCServer :: Methods -> IO ()
 runRHCServer methods = undefined
-
-data Request prm = Request {
-        resVersion :: String,
-        method :: String,
-        params :: Maybe prm,  -- structure
-        reqId :: Maybe String -- note: should change request type to sum type
-                              -- between notification and ordinary request
-}
-
-data Response res = Response {
-        reqVersion :: String,
-        result :: Maybe res, -- structure, determined structure by method
-        resError :: Maybe String, -- must be an object
-        resId :: Maybe String
-}
