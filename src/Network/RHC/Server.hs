29fac98870969590bf51c2149d8e8310b204d5e8
Dponya <ealtynbek089@gmail.com>
2022-07-27 22:41:51 +0600

    change Method function type

--- src/Network/RHC/Server.hs
+++ src/Network/RHC/Server.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
 module Network.RHC.Server where
 
 type MethodName = String
-type Method = * -> IO ()
-newtype Methods = Methods [(MethodName, Method)]
+newtype Method = Method { runMethod :: forall a b. a -> b }
+type Methods = [(MethodName, Method)]
+
+lookupMethod :: MethodName -> Methods -> Maybe Method
+lookupMethod = lookup
 
 runRHCServer :: Methods -> IO ()
 runRHCServer methods = undefined
