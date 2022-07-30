bc00dde8386886201544291ef19337f3dae37a07
Dponya <ealtynbek089@gmail.com>
2022-07-30 19:24:15 +0600

    add new type class RHCMethod that will return partly applied method

--- src/Network/RHC/Server.hs
+++ src/Network/RHC/Server.hs
@@ -1,9 +1,40 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Network.RHC.Server where
 
 type MethodName = String
-newtype Method = Method { runMethod :: forall a b. a -> b }
+
+data Key
+data Value
+
+newtype MethodArguments = MethodArguments [(Key, Value)]
+
+type MethodResult = IO (Either String String)
+
+newtype Method = Method {
+        runMethod :: MethodArguments -> MethodResult
+        }
+
 type Methods = [(MethodName, Method)]
+
+class RHCMethod a where
+        initMethod :: a -> MethodArguments -> MethodResult
+
+
+{-
+todo: replace Value with FlexibleInstances with
+        new constraint (class RHCTypes) that will type-cast aeson types to Value type
+-}
+instance RHCMethod b => RHCMethod (Value -> b) where
+        initMethod f (MethodArguments (x:xs)) =
+                initMethod (f (snd x)) (MethodArguments xs)
+        initMethod _ _ = return $ Left "too few arguments"
+
+instance RHCMethod (IO a) where
+        initMethod f (MethodArguments []) = do
+                                        _ <- f
+                                        return $ Right "good"
+        initMethod _ _ = return $ Left "too many arguments"
 
 lookupMethod :: MethodName -> Methods -> Maybe Method
 lookupMethod = lookup
