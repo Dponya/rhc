4f47a82b279ea4222579417bb57b66f78d367cfa
Dponya <ealtynbek089@gmail.com>
2022-08-06 23:31:28 +0600

    add new typeclass for AST parsing to custom datatype

--- src/Network/RHC/Internal/Server.hs
+++ src/Network/RHC/Internal/Server.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use newtype instead of data" #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Network.RHC.Internal.Server where
 
 import Network.Wai (
@@ -16,36 +15,39 @@ import Data.ByteString.Builder (byteString, Builder, toLazyByteString)
 import Data.ByteString.Lazy (toStrict, ByteString)
 import Data.Aeson (FromJSON, Object, decode, eitherDecode)
 import Data.Aeson.Types
-import Network.RHC.Server (Methods, MethodName, Method (Method), lookupMethod)
+import Network.RHC.Server (Methods, MethodName, Method (Method), lookupMethod, MethodArguments)
 import Data.Aeson.KeyMap (toList)
 
-runWarpServer :: Methods -> Port -> IO ()
-runWarpServer methods port =
+runWarpServer :: Port -> IO ()
+runWarpServer port =
                 let app :: Application
                     app req send =
                         do
                         let responseSender answer =
                                 send (responseBuilder status200 [] answer)
                         body <- requestBodyReceiver req
-                        print $ (eitherDecode :: ByteString -> Either String RequestRHC) 
-                                $ toLazyByteString body
                         responseSender body
                 in run port app
 
 requestBodyReceiver :: Request -> IO Builder
 requestBodyReceiver req = byteString . toStrict <$> lazyRequestBody req
 
-performingProcess :: Builder -> Methods -> Maybe Method
-performingProcess body methods = do
-                                   decoded <- decode $ toLazyByteString body
-                                   lookupMethod (method decoded) methods
+-- bodyRequestParse = parseMaybe (a -> Parser b) a
 
-data ParamsRHC = ArrayParams [(Key, Value)] deriving Show
+requestParsingProcess :: RequestParamsParser b => Builder -> Maybe b
+requestParsingProcess body = do
+                             requestRHC <- (decode :: ByteString -> Maybe RequestRHC)
+                                        $ toLazyByteString body
+                             paramsForParse <- params requestRHC
+                             requestParamsParse  paramsForParse
+
+class RequestParamsParser a where
+        requestParamsParse :: Object -> Maybe a
 
 data RequestRHC = RequestRHC {
         resVersion :: String,
         method :: MethodName,
-        params :: Maybe ParamsRHC,
+        params :: Maybe Object,
         reqId :: Maybe String
 } deriving Show
 
@@ -56,6 +58,7 @@ data ResponseRHC res = Response {
         resId :: Maybe String
 }
 
+
 instance FromJSON RequestRHC where
         parseJSON (Object v) =
                 RequestRHC <$> v .: "jsonrpc"
@@ -63,7 +66,3 @@ instance FromJSON RequestRHC where
                            <*> v .: "params"
                            <*> v .: "id"
         parseJSON _ = mempty
-
-instance FromJSON ParamsRHC where
-        parseJSON (Object v) = return $ ArrayParams $ Data.Aeson.KeyMap.toList v
-        parseJSON _ = undefined
--- src/Network/RHC/Server.hs
+++ src/Network/RHC/Server.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleInstances #-}
 module Network.RHC.Server where
+import Data.Aeson (Object)
 
 type MethodName = String
 
 data Key
-data Value
+data Value = VString String
+                | VInteger Integer
+                | VBool Bool
+                | VDouble Double
+                | VArray [Value]
+                | VObject [(String, Value)]
+                | VNull
+newtype MethodArguments = MethodArguments [Value]
 
-newtype MethodArguments = MethodArguments [(Key, Value)]
+margs = MethodArguments [VString "blah", VInteger 1]
+
+--newtype MethodArguments = MethodArguments [(Key, Value)]
 
 type MethodResult = IO (Either String String)
 
@@ -17,17 +27,19 @@ newtype Method = Method {
 
 type Methods = [(MethodName, Method)]
 
+method :: RHCMethod a => a -> Method
+method f = Method (initMethod f)
+
 class RHCMethod a where
         initMethod :: a -> MethodArguments -> MethodResult
-
 
 {-
 todo: replace Value with FlexibleInstances with
         new constraint (class RHCTypes) that will type-cast aeson types to Value type
 -}
-instance RHCMethod b => RHCMethod (Value -> b) where
+instance (RHCMethod b) => RHCMethod (Value -> b) where
         initMethod f (MethodArguments (x:xs)) =
-                initMethod (f (snd x)) (MethodArguments xs)
+                initMethod (f x) (MethodArguments xs)
         initMethod _ _ = return $ Left "too few arguments"
 
 instance RHCMethod (IO a) where
