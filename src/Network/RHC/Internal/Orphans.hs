4091e4b736cd3cf74c00582021d3ff4de80dbb3f
Dponya <ealtynbek089@gmail.com>
2022-10-13 15:14:10 +0600

    update gitignore & update cabal & add new module with Orphans

---
+++ src/Network/RHC/Internal/Orphans.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Network.RHC.Internal.Orphans where
+
+import Data.Aeson (ToJSON, FromJSON)
+import Language.Haskell.TH
+    ( Type, Name, NameSpace, Specificity, TyLit, TyVarBndr )
+import Language.Haskell.TH.Syntax
+    ( Type,
+      Name,
+      ModName,
+      NameFlavour,
+      NameSpace,
+      OccName,
+      PkgName,
+      Specificity,
+      TyLit,
+      TyVarBndr )
+
+deriving instance ToJSON Name
+deriving instance ToJSON Specificity
+deriving instance ToJSON (TyVarBndr Specificity)
+deriving instance ToJSON (TyVarBndr ())
+deriving instance ToJSON TyLit
+deriving instance ToJSON OccName
+deriving instance ToJSON PkgName
+deriving instance ToJSON NameSpace
+deriving instance ToJSON ModName
+deriving instance ToJSON NameFlavour
+deriving instance ToJSON Type
+
+deriving instance FromJSON Name
+deriving instance FromJSON Specificity
+deriving instance FromJSON (TyVarBndr Specificity)
+deriving instance FromJSON (TyVarBndr ())
+deriving instance FromJSON TyLit
+deriving instance FromJSON OccName
+deriving instance FromJSON PkgName
+deriving instance FromJSON NameSpace
+deriving instance FromJSON ModName
+deriving instance FromJSON NameFlavour
+deriving instance FromJSON Type
