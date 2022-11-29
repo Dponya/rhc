6a6cee6c30d2d1cc7b7e1b086fd5b8fd15fb10d0
Yerbol Altynbek <ealtynbek089@gmail.com>
2022-11-29 19:36:37 +0600

    add raw-strings-qq & refactor tests

--- test/Test/Rhc/Server.hs
+++ test/Test/Rhc/Server.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Test.Rhc.Server (serverSpec) where
 
-import Test.Hspec
-import Data.Aeson (Value, encode, ToJSON (toJSON))
 import Control.Monad.Catch (MonadThrow(throwM))
+import Data.Aeson (Value, encode, ToJSON (toJSON))
+import Test.Hspec
+import Text.RawString.QQ
 
 import Rhc
 
@@ -35,11 +37,14 @@ serverSpec = describe "mainThread" $ do
   context "executing single requests" $ do
     it "is single request with id was well replied with computed result" $ do
       let req =
-              "{\"id\":532,\
-                \\"jsonrpc\":\"2.0\",\
-                \\"method\":\"lists.maxLength\",\
-                \\"params\":[1,2,3,4]}"
-      -- let req = encode (FullReq $ ReqWithId "2.0" "lists.maxLength" params 532)
+              [r|
+                  {
+                    "id": 532,
+                    "jsonrpc": "2.0",
+                    "method": "lists.maxLength",
+                    "params": [1,2,3,4]
+                  }
+              |]
       let expected = toJSON $ ResSuccess "2.0" (toJSON @Integer 4 :: Value) 532
 
       sut <- mainThread req remoteTable
@@ -47,8 +52,12 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
 
     it "request with uncompleted structure is invalid" $ do
-      let req = "{ \"jsonrpc\": \"2.0\",\
-                    \\"method\": \"lists.concatWholeList\" }"
+      let req =
+            [r|
+              { "jsonrpc": "2.0",
+                "method": "lists.concatWholeList"
+              }
+            |]
       let errMsg = "The JSON sent is not a valid Request object."
       let expected = toJSON $
             ResErrsWithoutId "2.0"
@@ -59,7 +68,13 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
 
     it "request with uncorrect json syntax is invalid" $ do
-      let req = "{ \"jsonrpc\": \"2.0\", \"method\": \"lists.concatWholeList\" , }"
+      let req =
+            [r|
+              { "jsonrpc": "2.0",
+                "method": "lists.concatWholeList",
+                invalid
+              }
+            |]
       let errMsg = "Invalid JSON was received by the server. An error occurred on the server while parsing the JSON text."
       let expected = toJSON $
             ResErrsWithoutId "2.0"
@@ -70,10 +85,15 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
 
     it "request with method that doesn't exist is invalid" $ do
-      let req = "{\"id\":324,\
-                  \\"jsonrpc\":\"2.0\",\
-                  \\"method\":\"lists.imNotExist\",\
-                  \\"params\":[1,2,3]}"
+      let req =
+            [r|
+              {
+                "id": 324,
+                "jsonrpc": "2.0",
+                "method": "lists.imNotExist",
+                "params": [1,2,3]
+              }
+            |]
       let errMsg = "The method does not exist / is not available."
       let expected = toJSON $
             ResErrsWithId "2.0"
@@ -84,10 +104,15 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
 
     it "request with invalid params for method is invalid too" $ do
-      let req = "{\"id\":324,\
-                  \\"jsonrpc\":\"2.0\",\
-                  \\"method\":\"lists.maxLength\",\
-                  \\"params\":\"im not right\"}"
+      let req =
+            [r|
+              {
+                "id": 324,
+                "jsonrpc": "2.0",
+                "method": "lists.maxLength",
+                "params": "im not right"
+              }
+            |]
       let errMsg = "Invalid method parameter(s)."
       let expected = toJSON $
             ResErrsWithId "2.0"
@@ -98,10 +123,15 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
 
     it "method throws custom exception and replies client as well" $ do
-      let req = "{\"id\":1000,\
-                  \\"jsonrpc\":\"2.0\",\
-                  \\"method\":\"lists.checkMaxBound\",\
-                  \\"params\":52131}"
+      let req =
+            [r|
+              {
+                "id": 1000,
+                "jsonrpc": "2.0",
+                "method": "lists.checkMaxBound",
+                "params": 52131
+              }
+            |]
       let errMsg = "Number isn't a maxBound"
       let expected = toJSON $
             ResErrsWithId "2.0"
@@ -114,14 +144,23 @@ serverSpec = describe "mainThread" $ do
 
   context "executing batch requests" $ do
     it "is multiple requests with id was well replied all" $ do
-      let reqs = "[{\"id\":532,\
-              \\"jsonrpc\":\"2.0\",\
-              \\"method\":\"lists.maxLength\",\
-              \\"params\":[42,321,909]},\
-            \{\"id\":442,\
-              \\"jsonrpc\":\"2.0\",\
-              \\"method\":\"lists.concatWholeList\",\
-              \\"params\":[\"I'm \",\"the \",\"king!\"]}]"
+      let reqs = 
+            [r|
+              [
+                {
+                  "id": 532,
+                  "jsonrpc": "2.0",
+                  "method": "lists.maxLength",
+                  "params": [42,321,909]
+                },
+                {
+                  "id": 442,
+                  "jsonrpc": "2.0",
+                  "method": "lists.concatWholeList",
+                  "params": ["I'm ", "the ", "king!"]
+                }
+              ]
+            |]
       let expected = toJSON
             [ ResSuccess "2.0" (toJSON @Integer 909 :: Value) 532
             , ResSuccess "2.0" (toJSON @String "I'm the king!" :: Value) 442
@@ -132,14 +171,23 @@ serverSpec = describe "mainThread" $ do
       sut `shouldBe` (expected :: Value)
     
     it "method throws custom exception and replies client as well" $ do
-      let reqs = "[{\"id\":1000,\
-                    \\"jsonrpc\":\"2.0\",\
-                    \\"method\":\"lists.checkMaxBound\",\
-                    \\"params\":52131},\
-                  \{\"id\":1001,\
-                    \\"jsonrpc\":\"2.0\",\
-                    \\"method\":\"lists.checkMaxBound\",\
-                    \\"params\":342}]"
+      let reqs = 
+            [r|
+              [
+                {
+                  "id": 1000,
+                  "jsonrpc": "2.0",
+                  "method": "lists.checkMaxBound",
+                  "params": 52131
+                },
+                {
+                  "id": 1001,
+                  "jsonrpc": "2.0",
+                  "method": "lists.checkMaxBound",
+                  "params": 342   
+                }
+              ]
+            |]
       let errMsg = "Number isn't a maxBound"
       let expected = toJSON
             [ ResErrsWithId "2.0"
