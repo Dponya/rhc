d6d2d33b1f0b8cefb9c9b575ae87c9aac80ceb9e
dponya <dponya@pop-os.localdomain>
2022-11-29 19:07:39 +0600

    refactor verbose parser code

--- src/Rhc/Server/Request.hs
+++ src/Rhc/Server/Request.hs
@@ -12,33 +12,35 @@ module Rhc.Server.Request
   , handleRequest
   ) where
 
+import Control.Applicative ((<|>))
 import Control.Monad.Catch (MonadThrow(throwM))
 import Control.Monad.Reader (MonadReader(ask))
+import Control.Lens hiding ((.=))
+
 import Data.Aeson
   ( FromJSON(..)
-  , eitherDecode
-  , Result(..)
+  , ToJSON(toJSON)
+  , object
+  , decode
   , encode
-  , fromJSON
+  , (.=)
   , (.:)
   )
-import Data.Aeson (ToJSON(toJSON), object, (.=))
-import Data.Aeson.Types (Value(..), parseEither)
+import Data.Aeson.Lens (_JSON, values)
+import Data.Aeson.Types (Value(..), parseMaybe)
 import Data.ByteString.Lazy (ByteString)
+
 import Rhc.Server.Remote
   ( RemoteActionName
   , RPC
-  , ActionResponse
   , RemoteEnv(..)
   )
 import Rhc.Server.Error
-    ( ErrorExecutionCause(MethodNotFound)
-    , ErrorParseCause(ParseError, InvalidRequest)
-    )
+  ( ErrorExecutionCause(MethodNotFound)
+  , ErrorParseCause(ParseError, InvalidRequest)
+  )
 
 import qualified Data.Aeson.KeyMap as KM
-import qualified Data.Text as T
-import qualified Data.Vector as V
 
 data Req a
   = ReqNotif (Notif a) 
@@ -58,18 +60,22 @@ data ReqWithId a = ReqWithId
   , reqId :: Integer
   } deriving stock (Functor, Show)
 
-{- data Req
-  = Notif
-      { reqVersion :: String,
-        method :: RemoteActionName,
-        params :: Value
-      }
-  | Req
-      { reqVersion :: String,
-        method :: RemoteActionName,
-        params :: Value,
-        reqId :: Integer
-      } deriving stock Show -}
+instance ToJSON a => ToJSON (ReqWithId a) where
+  toJSON (ReqWithId {..}) =
+    object
+      [ "jsonrpc" .= ver
+      , "method"  .= mtd
+      , "params"  .= params
+      , "id"      .= reqId
+      ]
+
+instance ToJSON a => ToJSON (Notif a) where
+  toJSON (Notif {..}) =
+    object
+      [ "jsonrpc" .= ver
+      , "method"  .= mtd
+      , "params"  .= params
+      ]
 
 instance FromJSON (ReqWithId Value) where
   parseJSON (Object v) = ReqWithId
@@ -93,47 +99,9 @@ instance FromJSON (Req Value) where
     else ReqNotif <$> parseJSON @(Notif Value) jsn
   parseJSON _ = mempty
 
-{- instance FromJSON (Req Value) where
-  parseJSON (Object v) =
-    if KM.member "id" v
-      then FullReq $
-        ReqWithId
-          <$> v .: "jsonrpc"
-          <*> v .: "method"
-          <*> v .: "params"
-          <*> v .: "id"
-      else ReqNotif $
-        Notif
-          <$> v .: "jsonrpc"
-          <*> v .: "method"
-          <*> v .: "params"
-  parseJSON _ = mempty -}
-
 instance ToJSON (Req Value) where
-  toJSON (FullReq (ReqWithId {..})) =
-    object
-      [ "jsonrpc" .= ver
-      , "method"  .= mtd
-      , "params"  .= params
-      , "id"      .= reqId
-      ] 
-  toJSON (ReqNotif (Notif {..})) =
-    object
-      [ "jsonrpc" .= ver
-      , "method"  .= mtd
-      , "params"  .= params
-      ] 
-
-{- handleRequest :: Req Value -> RPC ActionResponse
-handleRequest req =
-  do
-    let action name table = maybe (throwM MethodNotFound) pure (lookup name table)
-    (RemoteEnv table) <- ask
-    f   <- action (method req) table
-    f (encode . params $ req) -}
-
-{- handleRequest :: Req Value -> RPC ActionResponse
-handleRequest = undefined -}
+  toJSON (FullReq req) = toJSON req
+  toJSON (ReqNotif req) = toJSON req
 
 handleRequest :: Req Value -> RPC (Req Value)
 handleRequest = \case
@@ -141,12 +109,12 @@ handleRequest = \case
     do (RemoteEnv table) <- ask
        f <- action mtd table
        val <- f (encode params)
-       pure $ const val <$> ReqNotif ntf
+       pure $ val <$ ReqNotif ntf
   FullReq full@(ReqWithId {..}) -> 
     do (RemoteEnv table) <- ask
        f <- action mtd table
        val <- f (encode params)
-       pure $ const val <$> FullReq full
+       pure $ val <$ FullReq full
   where
     action mtd table =
       maybe (throwM MethodNotFound)
@@ -154,28 +122,25 @@ handleRequest = \case
             (lookup mtd table)
 
 jsonToReq :: Value -> Maybe (Req Value)
-jsonToReq (Object o) =
-  do String ver <- KM.lookup "jsonrpc" o
-     String mtd <- KM.lookup "method" o
-     params <- KM.lookup "params" o
-     case KM.member "id" o of
-       False -> pure (ReqNotif $ Notif (T.unpack ver) mtd params)
-       True -> do
-        valId <- KM.lookup "id" o
-        case fromJSON valId of
-          Error _ -> Nothing
-          Success reqId -> pure (FullReq $ ReqWithId (T.unpack ver) mtd params reqId)
-jsonToReq _ = Nothing
+jsonToReq val = (full <&> FullReq) <|> (notif <&> ReqNotif)
+  where
+    full :: Maybe (ReqWithId Value)
+    full = val ^? _JSON
 
-traverseBatchReq :: Value -> [Maybe (Req Value)]
-traverseBatchReq (Array ar) = fmap jsonToReq (V.toList ar)
-traverseBatchReq _ = [Nothing]
+    notif :: Maybe (Notif Value)
+    notif = val ^? _JSON
 
-parseRequest :: ByteString -> RPC (Either [Maybe (Req Value)] (Req Value))
-parseRequest body = case eitherDecode @Value body of
-  Left _ -> throwM ParseError
-  Right val@(Array _) -> pure $ Left $ traverseBatchReq val
-  Right val@(Object _) -> case parseEither parseJSON val of
-      Left _ -> throwM InvalidRequest
-      Right obj -> pure $ Right obj
-  Right _ -> throwM ParseError
+batchReq :: Value -> [Maybe (Req Value)]
+batchReq val = val ^.. values . to jsonToReq
+
+parseRequest ::
+  ByteString 
+  -> RPC (Either [Maybe (Req Value)] (Req Value))
+parseRequest bs = decode @Value bs
+  & \case
+      Nothing -> throwM ParseError
+      Just val@(Array _) -> pure $ Left $ batchReq val
+      Just val@(Object _) -> parseMaybe parseJSON val & \case
+          Nothing -> throwM InvalidRequest
+          Just obj -> pure $ Right obj
+      Just _ -> throwM ParseError
